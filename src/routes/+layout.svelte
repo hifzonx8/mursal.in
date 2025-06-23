d1323cf12a17101cd3cbe6cb85425a56d1611d75
hifzonx8 <6242572+hifzonx8@users.noreply.github.com>
2025-06-23 22:33:18 +0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import "../app.css";
+
+  let { children } = $props();
+
+  type HeaderLink = {
+    title: string;
+    href: string;
+  };
+
+  const HeaderLinkList: HeaderLink[] = [
+    {
+      title: "main",
+      href: "/",
+    },
+    {
+      title: "document",
+      href: "/document",
+    },
+    {
+      title: "links",
+      href: "/links",
+    },
+  ];
+
+  console.log();
+</script>
+
+<div class="min-h-screen w-full bg-neutral-900 text-white">
+  <header class="bg-neutral-800 py-4 flex justify-center">
+    <nav class="flex space-x-4 text-xl">
+      {#each HeaderLinkList as link, i}
+        <a class="hover:underline" href={link.href}>{link.title}</a>
+      {/each}
+    </nav>
+  </header>
+  {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<div class="p-2 w-1/2">
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_static_element_interactions -->
+  <h1 class="text-7xl pb-5 select-none">mursal.in</h1>
+  <p class="m-2">
+    hello. i'm Hifzon Mursalin. as you might've probably guessed, i'm not that
+    good at designing ui, but i can create something that works, mostly behind
+    the scene. shameless <a
+      class="underline text-gray-300 hover:text-gray-400"
+      href="https://github.com/hifzonx8">github</a
+    >
+    plug? maybe
+    <a
+      class="underline text-gray-300 hover:text-gray-400"
+      href="https://www.linkedin.com/in/hifzon-mursalin-166943331/">linkedin</a
+    >?
+  </p>
+  <p class="m-2">
+    curious about my projects? i've built a webpage for viewing gold and silver
+    price. that's <a
+      class="underline text-gray-300 hover:text-gray-400"
+      href="https://dapoeremas.vercel.app/">dapoer emas</a
+    >. not much else but will add later :D
+  </p>
+</div>
